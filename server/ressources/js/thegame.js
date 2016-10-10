8db536fd64625b53b56046ca1dca7c00a84c2673
boudesit <boudesit@gmail.com>
2016-10-10 13:15:46 +0200

    add js duration & money

--- server/ressources/js/thegame.js
+++ server/ressources/js/thegame.js
@@ -3,6 +3,8 @@ var theGame = function(game) {
 	this.music = null;
   this.spriteBG = null;
 	this.white = "#FFFFFF";
+	this.money = null;
+	this.duration = null;
 }
 
 theGame.prototype = {
@@ -21,9 +23,15 @@ theGame.prototype = {
     		music.resume();
     	}
 
-		this.scoreText = this.game.add.text(20, 20, "" + "Test", { font: "30px Arial", fill: this.white});
+		this.scoreText = this.game.add.text(20, 200, "" + "Test", { font: "30px Arial", fill: this.white});
 		this.scoreText.fontWeight = "bold";
 		this.scoreText.setText("test1");
+
+		this.money = new Money(this.game);
+		this.money.create();
+
+		this.duration = new Duration(this.game);
+		this.duration.create();
 	},
 
 	update: function() {
@@ -34,6 +42,9 @@ theGame.prototype = {
 			{
 				var textrep = personality_insights(this.scoreText.text);
 				this.scoreText.setText(textrep);
+				this.money.update();
+				this.duration.update();
+
 			}
 
 	},
