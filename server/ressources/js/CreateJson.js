e29fbf269d6b2ae28e8f58035b1078ee0bb124e7
boudesit <boudesit@gmail.com>
2016-10-13 13:16:11 +0200

    update json

--- server/ressources/js/CreateJson.js
+++ server/ressources/js/CreateJson.js
@@ -28,6 +28,7 @@ CreateJson.prototype = {
       this.jsonMultiTab.columns = this.jsonColumns;
       this.jsonMultiTab.options = this.jsonOptions;
 
+      //
       var var1 = {
         "key": "name",
         "type": "text",
@@ -58,11 +59,70 @@ CreateJson.prototype = {
       this.jsonMultiTab.columns.push(var2);
       this.jsonMultiTab.columns.push(var3);
 
+      var var4 = {
+          "key": "1",
+          "name": "Perso 1",
+          "values": {
+          "name": "toto",
+          "LvlSkill": 50,
+          "skill": "Java"
+          }
+      };
+
+      var var5= {
+          "key": "1",
+          "name": "Perso 1",
+          "values": {
+          "name": "toto",
+          "LvlSkill": 50,
+          "skill": "Java"
+          }
+      };
+
+      var var6 = {
+          "key": "2",
+          "name": "Perso 2",
+          "values": {
+          "name": "titi",
+          "LvlSkill": 70,
+          "skill": "Java"
+          }
+      };
+
+      var var7 = {
+          "key": "3",
+          "name": "Perso 3",
+          "values": {
+          "name": "tata",
+          "LvlSkill": 65,
+          "skill": "Java"
+        }
+      };
+      var var8 = {
+          "key": "4",
+          "name": "Perso 4",
+          "values": {
+          "name": "tutu",
+          "LvlSkill": 55,
+          "skill": "C"
+          }
+    };
+
+    this.jsonMultiTab.options.push(var4);
+    this.jsonMultiTab.options.push(var5);
+    this.jsonMultiTab.options.push(var6);
+    this.jsonMultiTab.options.push(var7);
+    this.jsonMultiTab.options.push(var8);
+
       console.log(JSON.stringify(this.jsonMultiTab));
     },
 
 
     update: function() {
+    },
 
+    getJson : function() {
+
+      return JSON.stringify(this.jsonMultiTab);
     }
 }
